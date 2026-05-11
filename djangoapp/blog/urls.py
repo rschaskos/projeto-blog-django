dab51b9b93f13bc41cf24ac5cec06bcb7368d1d0
rschaskos <rschaskos@icloud.com>
2026-05-11 20:39:18 -0300

    feat(blog): implement pages and posts with templates
    Add page and post models with detail views, create templates for
    single page/post display, implement post card component, add
    pagination and meta information. Include admin customizations for
    content management.

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,8 +1,12 @@
+from blog.views import category, created_by, index, page, post
 from django.urls import path
-from blog.views import index
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/<slug:slug>/', post, name='post'),
+    path('page/<slug:slug>/', page, name='page'),
+    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
 ]
--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -2,7 +2,7 @@
 URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,14 +17,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('blog.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
 ]
 
-# essa configuração permite ver os arquivos estáticos enquanto o DEBUG estiver como TRUE
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
